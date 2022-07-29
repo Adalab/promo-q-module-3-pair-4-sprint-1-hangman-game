3be3f13932e7b38fc78defa648878b63d12f0350
SandMl3 <sandramartinlop@gmail.com>
2022-07-29 11:13:43 +0200

    Render solution letters, render error letters, add user letters array and paint a part of hangman for each user fail letter

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,26 @@ import "../styles/App.scss";
 import { useState } from "react";
 
 function App() {
-  const [numberOfErrors, setNumbersOfErrors] = useState(0);
-  const handleClick = () => {
-    setNumbersOfErrors(numberOfErrors + 1);
-    console.log(`dummy error-${numberOfErrors}`);
-  };
+  //const [numberOfErrors, setNumbersOfErrors] = useState(0);
+  const [word, setWord] = useState('katcroker');
+  const [userLetters, setUserLetters] = useState([]);
+  const wordLetters = word.split('');
+
+  const renderSolutionLetters = () => {
+    //return wordLetters.map(letter => <li class="letter"></li>)
+    return wordLetters.map((letter, i) => userLetters.includes(letter) ?<li key={i} className="letter">{letter}</li> :<li className="letter" key={i}></li>)
+  }
+
+ const renderErrorLetters = () => {
+  const failLetters = userLetters.filter(letter => !wordLetters.includes(letter));
+  return failLetters.map((letter, i) => <li key={i} className="letter">{letter}</li>);
+ }
+
+
+  // const handleClick = () => {
+  //   setNumbersOfErrors(numberOfErrors + 1);
+  //   console.log(`dummy error-${numberOfErrors}`);
+  // };
 
   const [lastLetter, setLastLetter] = useState("");
   const [alertMessage, setAlertMessage] = useState("");
@@ -14,8 +29,10 @@ function App() {
   const handelInputLetter = (ev) => {
     const inputValue = ev.currentTarget.value;
     const validatedLetter = /^[A-Za-z]+$/;
-    if (inputValue.match(validatedLetter) || !inputValue) {
+
+    if (inputValue.match(validatedLetter)) {
       setLastLetter(inputValue);
+      setUserLetters([...userLetters, inputValue]);
       setAlertMessage("");
     } else {
       setAlertMessage(" Debes introducir una letra");
@@ -29,11 +46,11 @@ function App() {
       </header>
       <main className="main">
         <section>
-          <button onClick={handleClick}>Incrementar</button>
+          {/* <button onClick={handleClick}>Incrementar</button> */}
           <div className="solution">
             <h2 className="title">Solución:</h2>
             <ul className="letters">
-              <li className="letter">k</li>
+              {/* <li className="letter">k</li>
               <li className="letter">a</li>
               <li className="letter"></li>
               <li className="letter">a</li>
@@ -42,17 +59,21 @@ function App() {
               <li className="letter"></li>
               <li className="letter">k</li>
               <li className="letter">e</li>
-              <li className="letter">r</li>
+              <li className="letter">r</li> */}
+              {renderSolutionLetters()}
             </ul>
           </div>
+
+
           <div className="error">
             <h2 className="title">Letras falladas:</h2>
             <ul className="letters">
-              <li className="letter">f</li>
+              {/* <li className="letter">f</li>
               <li className="letter">q</li>
               <li className="letter">h</li>
               <li className="letter">p</li>
-              <li className="letter">x</li>
+              <li className="letter">x</li> */}
+              {renderErrorLetters()}
             </ul>
           </div>
           <form className="form">
@@ -67,11 +88,12 @@ function App() {
               name="last-letter"
               id="last-letter"
               onChange={handelInputLetter}
+              //value = {lastLetter}
             />
             <p>{alertMessage}</p>
           </form>
         </section>
-        <section className={`dummy error-${numberOfErrors}`}>
+        <section className={`dummy error-${renderErrorLetters().length}`}>
           <span className="error-13 eye"></span>
           <span className="error-12 eye"></span>
           <span className="error-11 line"></span>
